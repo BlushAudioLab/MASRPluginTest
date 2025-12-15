78b6ef8e1a19b6efe88fb81da758d2efc2b87713
Blush Audio Dev <blushaudio@gmail.com>
2025-12-15 13:01:42 +0000

    Create new 'Square' class

---
+++ Source/Square.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Blush Audio Dev on 15/12/2025.
+//
+
+#include "Square.h"
---
+++ Source/Square.h
@@ -0,0 +1,13 @@
+//
+// Created by Blush Audio Dev on 15/12/2025.
+//
+
+#ifndef MRCOOLDUDE_SQUARE_H
+#define MRCOOLDUDE_SQUARE_H
+
+
+class Square {
+};
+
+
+#endif //MRCOOLDUDE_SQUARE_H
